7c11821abe38590ed0c6e96bf88cca14f3e3d29e
barse <barserkaua@gmail.com>
2021-04-14 22:58:58 +0300

    add models Category and Products

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)  # ім'я користувача
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)  # опис
+    image = models.ImageField(upload_to='category', blank=True)
+
+    class Meta:  # даний клас змінює назву
+        ordering = ('name',)  # упорядковує по імені
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)  # ім'я користувача
+    slug = models.SlugField(max_length=250, unique=True)  # як буде відображатися у нашій силці
+    description = models.TextField(blank=True)  # опис
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:  # даний клас змінює назву
+        ordering = ('name',)  # упорядковує по імені (щоб наші зміни вступили в силу
+        # їх потрібно мігрувати
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
